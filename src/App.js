02e50a64be599d13a5e6056dc9a8958905f2c872
kushagra <goelkushagra2000@gmail.com>
2022-10-23 23:36:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Header from "./component/Head/Header"
+import Features from "./component/Features/Features"
+import Home from "./component/Hero/Home"
+import Portfolio from "./component/Portfolio/Portfolio"
+
+
+import Contact from "./component/Contact/Contact"
+import Footer from "./component/Footer"
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Features />
+      <Portfolio />
+      
+      
+    
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Portfolio/Portfolio_data.js
@@ -0,0 +1,27 @@
+
+const Portfolio_data = [
+  {
+    id: 1,
+    category: "DEVELOPMENT",
+    totalLike: "600",
+    title: "Crypto Currency app ",
+    image: "./component/pickush/Crypto.png",
+    
+  },
+  {
+    id: 2,
+    category: "DEVELOPMENT",
+    totalLike: "750",
+    title: "E-Commerce Website",
+    image: "./image/portfolio-02.jpg",
+  },
+  {
+    id: 3,
+    category: "DEVELOPMENT",
+    totalLike: "630",
+    title: "Discord Clone",
+    image: "./image/portfolio-03.jpg",
+  },
+ 
+]
+export default Portfolio_data
